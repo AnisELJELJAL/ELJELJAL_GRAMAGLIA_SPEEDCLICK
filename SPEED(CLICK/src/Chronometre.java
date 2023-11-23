b267689ba6d75973c87a2e58e11ed4baf9f19503
mathi <mathi@HP-Mathis.lan>
2023-11-23 13:29:29 +0100

    Cette classe est la classe bouton, elle parametre les différents états des boutons. Opérationnelle

---
+++ SPEED(CLICK/src/Chronometre.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mathi
+ */
+public class Chronometre {
+    private long debut;
+    private long duree;
+
+    public void lancerChrono() {
+        debut = System.currentTimeMillis();
+    }
+
+    public void arreterChrono() {
+        long fin = System.currentTimeMillis();
+        duree = fin - debut;
+    }
+
+    public long tempsEcoule() {
+        return duree;
+    }
+
+    public long tempsRestant() {
+        return Math.max(0, duree - tempsEcoule());
+    }
+}
