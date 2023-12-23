2e4667fff4c092c8ab76cbee038af4e33429aa78
AnisELJELJAL <145330036+AnisELJELJAL@users.noreply.github.com>
2023-12-23 22:34:03 +0100

    Update Partie.java
    lance la partie et ses conditions

--- src/SPEEDCLICKP/Partie.java
+++ src/SPEEDCLICKP/Partie.java
@@ -9,7 +9,9 @@ package SPEEDCLICKP;
  */
 
 /**
- *
+ * La classe Partie représente une session de jeu dans Speed-Click.
+ * Les joueurs doivent cliquer sur des boutons allumés dans une grille pendant une durée définie.
+ * Le but est d'obtenir le meilleur score possible dans le temps imparti.
  * @author anis_
  */
 public class Partie {
@@ -19,63 +21,77 @@ public class Partie {
 
     public Partie(int nbLignes, int nbColonnes, int dureePartieMillis) {
         grille = new Grille(nbLignes, nbColonnes);
-        chronometre = new Chronometre();
-        chronometre.setDuree(dureePartieMillis);
+       
+        
         enCours = false;
     }
 
-//    public void demarrerPartie() {
-//        if (!enCours) {
-//            grille.eteindreTousLesBoutons();
-//            grille.allumerBoutonAleatoire();
+    /**
+     * lance la partie
+     */
+    public void demarrerPartie() {
+        if (!enCours) {
+            grille.eteindreTousLesBoutons();
+            grille.allumerBoutonAleatoire();
 //            chronometre.lancerChrono();
-//            enCours = true;
-//            afficherGrille();
-//        } else {
-//            System.out.println("La partie est déjà en cours !");
-//        }
-//    }
+            enCours = true;
+            afficherGrille();
+        } else {
+            System.out.println("La partie est déjà en cours !");
+        }
+    }
 
-//    public void arreterPartie() {
-//        if (enCours) {
+    /**
+     * arrete la partie
+     */
+    public void arreterPartie() {
+        if (enCours) {
 //            chronometre.arreterChrono();
-//            enCours = false;
-//            System.out.println("Partie terminée. Score final : " + grille.getScore());
-//            System.out.println("Temps écoulé : " + (chronometre.tempsEcoule() / 1000) + " secondes");
-//        } else {
-//            System.out.println("Aucune partie en cours !");
-//        }
-//    }
+            enCours = false;
+            System.out.println("Partie terminée. Score final : " + grille.getScore());
+            System.out.println("Temps écoulé : " + (chronometre.tempsEcoule() / 1000) + " secondes");
+        } else {
+            System.out.println("Aucune partie en cours !");
+        }
+    }
 
-//    public void clicBouton(int ligne, int colonne) {
-//        if (enCours) {
-//            grille.boutonClique();
-//            afficherGrille();
-//
-//            if (grille.getScore() > 0 && grille.getScore() % 5 == 0) {
-//            }
-//
-//            if (grille.getScore() > 0 && grille.getScore() % 10 == 0) {
-//            }
-//
-//            if (grille.getScore() > 0 && grille.getScore() % 15 == 0) {
-//            }
-//
-//            if (grille.estBoutonAllume(ligne, colonne)) {
-//               
-//                grille.eteindreBouton(ligne, colonne);
-//                System.out.println("Erreur de clic ! Score diminué.");
-//            }
-//            else {
-//                grille.allumerBouton(ligne,colonne);
-//            }
-//
-//            grille.allumerBoutonAleatoire();
-//        } else {
-//            System.out.println("Aucune partie en cours !");
-//        }
-//    }
+    /**
+     * score lorsque l'ont clique
+     * @param ligne
+     * @param colonne
+     */
+    public void clicBouton(int ligne, int colonne) {
+        if (enCours) {
+            grille.boutonClique();
+            afficherGrille();
 
+            if (grille.getScore() > 0 && grille.getScore() % 5 == 0) {
+            }
+
+            if (grille.getScore() > 0 && grille.getScore() % 10 == 0) {
+            }
+
+            if (grille.getScore() > 0 && grille.getScore() % 15 == 0) {
+            }
+
+            if (grille.estBoutonAllume(ligne, colonne)) {
+               
+                grille.eteindreBouton(ligne, colonne);
+                System.out.println("Erreur de clic ! Score diminué.");
+            }
+            else {
+                grille.allumerBouton(ligne,colonne);
+            }
+
+            grille.allumerBoutonAleatoire();
+        } else {
+            System.out.println("Aucune partie en cours !");
+        }
+    }
+
+    /**
+     * affiche la grille 
+     */
     public void afficherGrille() {
         System.out.println(grille);
     }
